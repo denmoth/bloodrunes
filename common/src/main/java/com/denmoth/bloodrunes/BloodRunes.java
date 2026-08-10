4a1dd8857d56c8daf7f05aa5f69b56c7043e5043
Denmoth <149831362+denmoth@users.noreply.github.com>
2026-08-10 23:14:02 +0300

    Initial commit for BloodRunes mod

---
+++ common/src/main/java/com/denmoth/bloodrunes/BloodRunes.java
@@ -0,0 +1,9 @@
+package com.denmoth.bloodrunes;
+
+public class BloodRunes {
+    public static final String MOD_ID = "bloodrunes";
+
+    public static void init() {
+        // Common initialization
+    }
+}
---
+++ neoforge/src/main/java/com/denmoth/bloodrunes/neoforge/BloodRunesNeoForge.java
@@ -0,0 +1,11 @@
+package com.denmoth.bloodrunes.neoforge;
+
+import com.denmoth.bloodrunes.BloodRunes;
+import net.neoforged.fml.common.Mod;
+
+@Mod(BloodRunes.MOD_ID)
+public class BloodRunesNeoForge {
+    public BloodRunesNeoForge() {
+        BloodRunes.init();
+    }
+}
